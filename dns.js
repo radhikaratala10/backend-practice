4e48af5cb8bf3fa561406e58e51d4226b8f95301
radhikaratala10 <radhikaratala10@gmail.com>
2026-03-20 19:24:24 +0530

    first commit

--- dns.js
+++ dns.js
@@ -11,7 +11,7 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose.connect(process.env.MONGO_ATLS)
 .then(()=>{
  console.log("MongoDB connected")
 })
